3e7105a953a30f664962e5371cb590d64f06ace2
Texposse <christophermuster@yahoo.co.uk>
2025-03-11 20:11:01 +0000

    main - changed title placeholder

--- app/page-components/code-challenges/code-challenges.tsx
+++ app/page-components/code-challenges/code-challenges.tsx
@@ -17,12 +17,12 @@ export default function CodeChallenges() {
             <RPSLSGame />
           </AccordionTab>
           <AccordionTab title="Project 2">
-            <h2 className="text-3xl">Project 2</h2>
-            <p>Project 2 content</p>
+            <h2 className="text-3xl">Challenge 2</h2>
+            <p>Challenge 2 content</p>
           </AccordionTab>
           <AccordionTab title="Project 3">
-            <h2 className="text-3xl">Project 3</h2>
-            <p>Project 3 content</p>
+            <h2 className="text-3xl">Challenge 3</h2>
+            <p>Challenge 3 content</p>
           </AccordionTab>
         </Accordion>
       </div>
